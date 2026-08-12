9df4f29ef7dbe6a2d4793884d4c6151f86259692
Abdullah Ramzan <your-email@example.com>
2026-08-12 23:17:18 +0500

    add refresh token functionality and update user routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { apiError } from "../utils/apiError.js";
 import { User } from "../models/user.model.js";
 import { fileUploadCloudinary } from "../utils/cloudinary.js";
 import { apiResponse } from "../utils/apiResponse.js";
+import jwt from "jsonwebtoken"
 
 const generateAccessAndRefreshToken = async(userId)=>{
     try {
@@ -112,7 +113,7 @@ const loginUser = asyncHandler(async (req,res)=>{
     // take the data from postman/frontend
     const {email,username,password} = req.body
     // check if empty
-    if(!username || !email) {
+    if(!username && !email) {
         throw new apiError(400,"username or email is required")
     }
     //find the user by email/username
@@ -132,7 +133,7 @@ const loginUser = asyncHandler(async (req,res)=>{
 
  
     //now we receive access , refresh token by destructuring
-    const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id) 
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
     
     // get the user from mongo db but remove their password , reftoken 
     const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
@@ -148,7 +149,7 @@ const loginUser = asyncHandler(async (req,res)=>{
     .cookie("accessToken", accessToken, options)
     .cookie("refreshToken", refreshToken, options)
     .json(
-        new ApiResponse( // this response is for frontend
+        new apiResponse( // this response is for frontend
             200, 
             {
                 user: loggedInUser, accessToken, refreshToken
@@ -181,14 +182,59 @@ const logoutUser = asyncHandler(async(req, res) => {
     .status(200)
     .clearCookie("accessToken", options)
     .clearCookie("refreshToken", options)
-    .json(new ApiResponse(200, {}, "User logged Out"))
+    .json(new apiResponse(200, {}, "User logged Out"))
 })
 
+const refreshAccessToken = asyncHandler(async(req,res)=>{
+   const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+   if(!incomingRefreshToken){
+    throw new apiError(401, "unauthorized request")
+   }
+
+   try {
+    const decodedToken = jwt.verify(
+     incomingRefreshToken,
+     process.env.REFRESH_TOKEN_SECRET
+    )
+ 
+    const user = await User.findById(decodedToken?._id)
+ 
+    if(!user) throw new apiError(401,"invalid refresh token")
+ 
+     if (incomingRefreshToken !== user?.refreshToken) {
+         throw new apiError(401,"refresh token is expired or used")
+     }
+  
+     const options = {
+         httpOnly : true,
+         secure : true
+     }
+ 
+    const {accessToken,newRefreshToken} = await generateAccessAndRefreshToken(user._id)
+ 
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken , options)
+    .cookie("refreshToken",newRefreshToken , options)
+    .json(
+     new apiResponse(
+         200,
+         {accessToken,refreshToken : newRefreshToken},
+         "access token refreshed "
+ 
+     )
+    )
+   } catch (error) {
+    throw new apiError(400 , "generation of access token failed")
+   }
+
+})
 export {
     registerUser,
     loginUser,
     logoutUser,
-
+    refreshAccessToken,
 }
 
 //access token = temporary key and refresh token = key used to get a new temporary key.
