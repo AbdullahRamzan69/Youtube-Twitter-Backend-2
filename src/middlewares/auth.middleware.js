9c9651c68ff820ccf73d551bf774b026f730dbe3
Abdullah Ramzan <your-email@example.com>
2026-08-13 14:41:23 +0500

    add subscribtion model

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 import { User } from "../models/user.model.js";
 
-export const verifyJWT = asyncHandler(async(req, _, next) => {
+export const verifyJWT = asyncHandler(async(req, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
         
@@ -32,3 +32,4 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
 // this middlewares purpose
 //Check whether the incoming request belongs to a logged-in user.
 // If it does, put that user into req.user and allow the request to continue.
+//The user sends their Access Token with every request. `auth.middleware.js` verifies it.
---
+++ src/models/subscribtion.model.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscribtionSchema = new Schema({
+    subscriber:{
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    },
+    channel:{
+         type: mongoose.Types.ObjectId,
+        ref: "User"
+    }
+
+},{timestamps:true})
+
+export const Subscribtion = mongoose.model("subscribtion", subscribtionSchema)
