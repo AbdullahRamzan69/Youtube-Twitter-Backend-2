c04a7fd35362b69b2432967fbf715b8720bd22ed
Abdullah Ramzan <your-email@example.com>
2026-08-14 18:16:58 +0500

    add account update and image upload functionalities in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -233,32 +233,135 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
     throw new apiError(400 , "generation of access token failed")
    }
 
-   const changePassword = asyncHandler(async(req,res)=>{
-    
-    const {oldPassword,newPassword} = req.body
+})
 
-    const user = await User.findById(req.user?.id)
+   const changePassword = asyncHandler(async (req, res) => {
+
+    const { oldPassword, newPassword } = req.body
+
+    const user = await User.findById(req.user?._id)
 
     const correctPassword = await user.isPasswordCorrect(oldPassword)
 
-    if(!correctPassword){
-        throw new apiError(400,"Your given password doesnt match with old password")
+    if (!correctPassword) {
+        throw new apiError(
+            400,
+            "Your given password doesn't match the old password"
+        )
     }
+
     user.password = newPassword
-    user.save({validateBeforeSave: false})
-   })
-   return res
-   .status(200)
-   .json( new apiResponse(200,
-    {},
-    "password has been changed"
-   ))
+
+    await user.save({
+        validateBeforeSave: false
+    })
+
+    return res
+        .status(200)
+        .json(
+            new apiResponse(
+                200,
+                {},
+                "Password has been changed"
+            )
+        )
 })
 
 const getCurrentUser = asyncHandler(async(req,res)=>{
     return res
     .status(200)
-    .json(200,req.user,"current user fetched successfully")
+    .json( new apiResponse( 200,req.user,"current user fetched successfully"))
+})
+
+const updateAccountDetails = asyncHandler(async(req, res) => {
+    const {fullName, email} = req.body
+
+    if (!fullName || !email) {
+        throw new apiError(400, "All fields are required")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName,
+                email: email
+            }
+        },
+        {new: true}
+        
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new apiResponse(200, user, "Account details updated successfully"))
+});
+
+const updateUserAvatar = asyncHandler(async(req, res) => {
+    const avatarLocalPath = req.file?.path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is missing")
+    }
+
+    //TODO: delete old image - assignment
+
+    const avatar = await fileUploadCloudinary(avatarLocalPath)
+
+    if (!avatar.url) {
+        throw new ApiError(400, "Error while uploading on avatar")
+        
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                avatar: avatar.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Avatar image updated successfully")
+    )
+})
+
+const updateUserCoverImage = asyncHandler(async(req, res) => {
+    const coverImageLocalPath = req.file?.path
+
+    if (!coverImageLocalPath) {
+        throw new ApiError(400, "Cover image file is missing")
+    }
+
+    //TODO: delete old image - assignment
+
+
+    const coverImage = await fileUploadCloudinary(coverImageLocalPath)
+
+    if (!coverImage.url) {
+        throw new ApiError(400, "Error while uploading on avatar")
+        
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                coverImage: coverImage.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Cover image updated successfully")
+    )
 })
 
 export {
@@ -268,6 +371,9 @@ export {
     refreshAccessToken,
     changePassword,
     getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
 }
 
 //access token = temporary key and refresh token = key used to get a new temporary key.
