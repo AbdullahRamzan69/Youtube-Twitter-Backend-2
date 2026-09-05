58d81b823a5d195102431cb01db4667700a2d6e9
Abdullah Ramzan <your-email@example.com>
2026-09-05 17:05:50 +0500

    feat: add like functionality to tweets and update response structure

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,4 +1,5 @@
 import { Tweet } from "../models/tweet.model.js";
+import { Like } from "../models/like.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
@@ -12,14 +13,53 @@ const createTweet = asyncHandler(async (req, res) => {
     }
 
     // 2. Create the tweet
-    const tweet = await Tweet.create({
+    const createdTweet = await Tweet.create({
         content,
         owner: req.user._id
     });
 
+    const tweet = await Tweet.findById(createdTweet._id).populate("owner", "username avatar fullName");
+
     // 3. Send response
     return res.status(201).json(
-        new apiResponse(201, tweet, "Tweet created successfully")
+        new apiResponse(
+            201, 
+            {
+                ...tweet.toObject(),
+                likesCount: 0,
+                isLiked: false
+            }, 
+            "Tweet created successfully"
+        )
+    );
+});
+
+const getAllTweets = asyncHandler(async (req, res) => {
+    const tweets = await Tweet.find()
+        .populate("owner", "username avatar fullName")
+        .sort({ createdAt: -1 });
+
+    const tweetsWithLikes = await Promise.all(
+        tweets.map(async (tweet) => {
+            const likesCount = await Like.countDocuments({ tweet: tweet._id });
+            let isLiked = false;
+            if (req.user) {
+                const userLike = await Like.findOne({
+                    tweet: tweet._id,
+                    likedBy: req.user._id
+                });
+                isLiked = !!userLike;
+            }
+            return {
+                ...tweet.toObject(),
+                likesCount,
+                isLiked
+            };
+        })
+    );
+
+    return res.status(200).json(
+        new apiResponse(200, tweetsWithLikes, "Tweets fetched successfully")
     );
 });
 
@@ -28,11 +68,32 @@ const getUserTweets = asyncHandler(async (req, res) => {
     const { userId } = req.params;
 
     // 2. Find all tweets by this user
-    const tweets = await Tweet.find({ owner: userId }).sort({ createdAt: -1 });
+    const tweets = await Tweet.find({ owner: userId })
+        .populate("owner", "username avatar fullName")
+        .sort({ createdAt: -1 });
+
+    const tweetsWithLikes = await Promise.all(
+        tweets.map(async (tweet) => {
+            const likesCount = await Like.countDocuments({ tweet: tweet._id });
+            let isLiked = false;
+            if (req.user) {
+                const userLike = await Like.findOne({
+                    tweet: tweet._id,
+                    likedBy: req.user._id
+                });
+                isLiked = !!userLike;
+            }
+            return {
+                ...tweet.toObject(),
+                likesCount,
+                isLiked
+            };
+        })
+    );
 
     // 3. Send response
     return res.status(200).json(
-        new apiResponse(200, tweets, "Tweets fetched successfully")
+        new apiResponse(200, tweetsWithLikes, "User tweets fetched successfully")
     );
 });
 
@@ -61,9 +122,11 @@ const updateTweet = asyncHandler(async (req, res) => {
     tweet.content = content;
     await tweet.save();
 
+    const updatedTweet = await Tweet.findById(tweetId).populate("owner", "username avatar fullName");
+
     // 5. Send response
     return res.status(200).json(
-        new apiResponse(200, tweet, "Tweet updated successfully")
+        new apiResponse(200, updatedTweet, "Tweet updated successfully")
     );
 });
 
@@ -83,8 +146,9 @@ const deleteTweet = asyncHandler(async (req, res) => {
         throw new apiError(403, "You cannot delete this tweet");
     }
 
-    // 4. Delete from database
+    // 4. Delete from database and clean associated likes
     await Tweet.findByIdAndDelete(tweetId);
+    await Like.deleteMany({ tweet: tweetId });
 
     // 5. Send response
     return res.status(200).json(
@@ -94,7 +158,9 @@ const deleteTweet = asyncHandler(async (req, res) => {
 
 export {
     createTweet,
+    getAllTweets,
     getUserTweets,
     updateTweet,
     deleteTweet
 };
+
